8d0c4d38619a2230030b43bb782e88e4715dc215
Britt Mathis <britt.mathis@gmail.com>
2015-03-23 16:22:20 -0400

    Empty GoogleDriveController

---
+++ Nubulous/Nubulous/Controllers/GoogleDriveController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v2;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+
+namespace Nubulous.Controllers
+{
+    public class GoogleDriveController : Controller
+    {
+        // GET: GoogleDrive
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
