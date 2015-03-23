936b73756f3e406d5c30b3893d66ed2e22dfd7fe
Britt Mathis <britt.mathis@gmail.com>
2015-03-23 17:17:16 -0400

    Code from google's example

---
+++ Nubulous/Nubulous/AppFlowMetadata.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Auth.OAuth2.Mvc;
+using Google.Apis.Drive.v2;
+using Google.Apis.Util.Store;
+using System.Web.Mvc;
+
+namespace Nubulous
+{
+    public class AppFlowMetadata : FlowMetadata
+    {
+        private static readonly IAuthorizationCodeFlow flow =
+            new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
+            {
+                ClientSecrets = new ClientSecrets
+                {
+                    ClientId = ConfigurationManager.AppSettings.Get("GOOGLE_CLIENT_ID"),
+                    ClientSecret = ConfigurationManager.AppSettings.Get("GOOGLE_CLIENT_SECRET"),
+                },
+                Scopes = new[] { DriveService.Scope.Drive },
+                DataStore = new FileDataStore("Drive.Api.Auth.Store")
+            });
+
+        public override string GetUserId(Controller controller)
+        {
+            var user = controller.Session["user"];
+            if (user == null)
+            {
+                user = Guid.NewGuid();
+                controller.Session["user"] = user;
+            }
+            return user.ToString();
+        }
+
+        public override IAuthorizationCodeFlow Flow
+        {
+            get
+            {
+                return flow;
+            }
+        }
+    }
+}
